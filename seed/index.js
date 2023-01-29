c1f506b22a356cf61a1e58f14fe979f12736c81b
Gerard Solanes <gerardsolher@gmail.com>
2023-01-29 13:05:53 +0100

    Creates seed script for albums

--- seed/index.js
+++ seed/index.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-const Show = require('../models/Album');
 const MONGO_URL = process.env.MONGO_URL;
-const shows = require('../data/albums');
+const albums = require('../data/albums');
+const Album = require('../models/Album');
+
+mongoose.connect(MONGO_URL)
+    .then(response => console.log(`Connected to DB ${response.connection.name}`))
+    .then(() => Album.deleteMany())
+    .then(() => {
+        return Album.create(albums);
+    })
+    .then(createdAlbums => console.log(`Inserted ${createdAlbums.length} in the DB`))
+    .then(() => mangoose.connection.close())
+    .catch(err => console.error(err));
