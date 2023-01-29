c6d018b2991ef9058c1c299eeb41335956832d16
Marc Bertran <mbertransuarez@gmail.com>
2023-01-29 16:19:56 +0100

    logout

--- routes/auth.js
+++ routes/auth.js
@@ -89,7 +89,7 @@ router.post("/login", async (req, res, next) => {
 // @desc    Destroy user session and log out
 // @route   POST /auth/logout
 // @access  Private
-router.post("/logout", (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   req.session.destroy((err) => {
     if (err) {
       next(err);
