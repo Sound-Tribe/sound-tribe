0bd0c7082678c574b99baa1aeb1c825db8392d5f
Gerard Solanes <gerardsolher@gmail.com>
2023-01-30 19:28:31 +0100

    Creates profile layout

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { toLocaleString } = require('../data/albums.js');
 const Album = require('../models/Album.js');
 const User = require('../models/User.js');
 
---
+++ routes/profile.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const app = require('../app.js');
+const Album = require('../models/Album.js');
+const User = require('../models/User.js');
+
+
+
+module.exports = router;
