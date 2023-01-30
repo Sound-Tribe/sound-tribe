b215cac8f945e71ffab3d30fa0ea39035268fd29
Gerard Solanes <gerardsolher@gmail.com>
2023-01-30 20:31:25 +0100

    Adds calendar route to profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -48,4 +48,26 @@ router.get('/liked', isLoggedIn, async (req, res, next) => {
     }
 });
 
+// @desc    Profile Page. Content = Calendar
+// @route   GET /profile/calendar
+// @access  Private
+router.get('/calendar', isLoggedIn, async (req, res, next) => {
+    const userId = req.session.currentUser._id;
+    try {
+        const user = await User.findById(userId);
+        // Should retreive all liked posts from user
+        // For testing purposes
+        const content =[{
+            date: 'Wednesday 1st',
+            location: 'Barcelona'
+        },{
+            date: 'Thursday 2nd',
+            location: 'Valencia'
+        }];
+        res.render('profile/profile', {user, calendar: content});
+    } catch (error) {
+        next(error);
+    }
+})
+
 module.exports = router;
