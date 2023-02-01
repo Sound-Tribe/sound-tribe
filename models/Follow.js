284296240c871d3d0044258d4a197bead865172a
Gerard Solanes <gerardsolher@gmail.com>
2023-02-01 09:24:23 +0100

    Creates follow model

---
+++ models/Follow.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+
+const followSchema = new Schema({
+    followerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    followeeId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Follow = mongoose.model('Follow', followSchema);
+module.exports = Follow;
