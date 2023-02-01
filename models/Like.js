e89b8523e93365636ee71135604608169f02a81f
Marc Bertran <mbertransuarez@gmail.com>
2023-02-01 13:15:58 +0100

    like model

---
+++ models/Like.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const likeSchema = new Schema({
+    albumId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Album'
+    },
+    likeUserId: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User'
+    },
+    isLiked: {
+        type: Number,
+        default: 0,
+    }
+}, {
+    timestamps: true
+});
+
+const Like = mongoose.model('Like', likeSchema);
+module.exports = Like;
