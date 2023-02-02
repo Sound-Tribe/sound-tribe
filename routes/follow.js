51f585fafc493e7952beccf2dc0fe611fe9ea2e0
Gerard Solanes <gerardsolher@gmail.com>
2023-02-02 09:46:29 +0100

    Adds unfollow functionality:

--- routes/follow.js
+++ routes/follow.js
@@ -11,16 +11,32 @@ router.post('/new/:followeeId', isLoggedIn, async (req, res, next) => {
     const followerId = req.session.currentUser._id;
     try {
         const existentFollow = await Follow.find({ followeeId: followeeId, followerId: followerId });
-        console.log(existentFollow);
         if (existentFollow.length != 0) {
-            //redirect to unfollow
+            res.redirect(`/follow/delete/${followeeId}`);
         } else {
             await Follow.create({ followeeId: followeeId, followerId: followerId });
             await User.findOneAndUpdate({_id :followeeId}, {$inc : {'followers' : 1}});
-            const updatedUser = await User.findOneAndUpdate({_id :followerId}, {$inc : {'following' : 1}}, {new: true});
-            res.session.currentUser = updatedUser;
-            res.redirect(req.originalUrl);
+            const updatedUser = await User.findOneAndUpdate({_id :followerId}, {$inc : {'following' : 1}}, { new: true });
+            req.session.currentUser = updatedUser;
+            res.redirect(`/profile/view/${followeeId}/posts`);
         }
+    } catch (error) {
+        next(error);
+    }
+});
+
+// @desc    Deletes an existing follow relation
+// @route   POST /follow/delete/:followeeId
+// @access  Private
+router.get('/delete/:followeeId', isLoggedIn, async (req, res, next) => {
+    const { followeeId } = req.params;
+    const followerId = req.session.currentUser._id;
+    try {
+        await Follow.findOneAndDelete({ followeeId: followeeId, followerId: followerId });
+        await User.findOneAndUpdate({_id :followeeId}, {$inc : {'followers' : -1}});
+        const updatedUser = await User.findOneAndUpdate({_id :followerId}, {$inc : {'following' : -1}}, {new: true});
+        req.session.currentUser = updatedUser;
+        res.redirect(`/profile/view/${followeeId}/posts`);
     } catch (error) {
         next(error);
     }
